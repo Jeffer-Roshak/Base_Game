9b3a6b01f280e296fd1a9fc229d0d8bb2ef8cf7d
Fayaz Haneefa <100049751@ku.ac.ae>
2022-04-24 02:16:11 +0400

    Base Game Complete. Missing: Inventory System NPCs Game Events Item Descriptions Linux Input System

--- Base_Game/Base_Game.py
+++ Base_Game/Base_Game.py
@@ -1,4 +1,4 @@
-credits='''
+title='''
                                                                                                              
 `7MM"""Yb.      db      `7MM"""Mq.  `7MMF' `YMM'     `7MMF'     A     `7MF' db   MMP""MM""YMM `7MM"""YMM  `7MM"""Mq.  
   MM    `Yb.   ;MM:       MM   `MM.   MM   .M'         `MA     ,MA     ,V  ;MM:  P'   MM   `7   MM    `7    MM   `MM. 
@@ -7,14 +7,24 @@ credits='''
   MM     ,MP AbmmmqMA     MM  YM.     MM  VMA            `MM A'  `MM A'  AbmmmqMA     MM        MM   Y  ,   MM  YM.   
   MM    ,dP'A'     VML    MM   `Mb.   MM   `MM.           :MM;    :MM;  A'     VML    MM        MM     ,M   MM   `Mb. 
 .JMMmmmdP'.AMA.   .AMMA..JMML. .JMM..JMML.   MMb.          VF      VF .AMA.   .AMMA..JMML.    .JMMmmmmMMM .JMML. .JMM.
-                                                                                                                      
+
+'''
+credits='''
 PROJECT:// Dark Water
 Code by: _SPECTRE
+'''
+
+'''
 Current Issues\Points to Consider:
 *Functions that take in map as an argument, unnecessary with global variable map_Current
+ASCII font is Georgia11
 '''
+
 import numpy as np
 import msvcrt #Only for windows
+import time
+import sys
+import os
 
 #Variables
 '''Maps:
@@ -58,7 +68,7 @@ map_BioChem = np.array([[4, 0, 0, 0],
                         [0, 4, 4, 1],
                         [4, 10, 0, 2]])
 
-doors = [{'locked':True, 'f_room':1, 't_room':0, 'f_x':0, 'f_y':0, 't_x':1, 't_y':2}, #Prep1-Dock1
+doors = [{'locked':False, 'f_room':1, 't_room':0, 'f_x':0, 'f_y':0, 't_x':1, 't_y':2}, #Prep1-Dock1
          {'locked':False, 'f_room':1, 't_room':3, 'f_x':0, 'f_y':2, 't_x':5, 't_y':1}, #Prep1-Living
          {'locked':False, 'f_room':1, 't_room':2, 'f_x':0, 'f_y':4, 't_x':1, 't_y':0}, #Prep1-MedBay
          {'locked':False, 'f_room':4, 't_room':3, 'f_x':2, 'f_y':1, 't_x':0, 't_y':1}, #CIC-Living
@@ -80,7 +90,7 @@ storage = [
   {'name':"Equipment Locker",'itemID':0,'room':1,'row':2,'column':0},
   {'name':"Equipment Locker",'itemID':1,'room':1,'row':2,'column':1},
   {'name':"Coffee Maker",'itemID':7,'room':1,'row':2,'column':3},
-  {'name':"Pantry",'itemID':255,'room':1,'row':2,'column':4},
+  {'name':"Pantry",'itemID':35,'room':1,'row':2,'column':4},
 
   {'name':"Tony's Table",'itemID':23,'room':2,'row':0,'column':1},
   {'name':"Medical Locker",'itemID':3,'room':2,'row':0,'column':4},
@@ -99,14 +109,27 @@ storage = [
 
   {'name':"Freezer",'itemID':13,'room':7,'row':0,'column':0},
   {'name':"Supply Closet",'itemID':2,'room':7,'row':2,'column':0},
-  {'name':"a Table",'itemID':255,'room':7,'row':1,'column':1},
-  {'name':"Table",'itemID':255,'room':7,'row':1,'column':2}
+  {'name':"Table",'itemID':35,'room':7,'row':1,'column':1},
+  {'name':"Table",'itemID':35,'room':7,'row':1,'column':2}
 ]
 
 safes = [
-    {'name':"Safe",'code':'1234','locked':True,'itemID':6,'room':1,'row':2,'column':3}
+    {'name':"Safe",'code':'0451','locked':True,'itemID':6,'room':2,'row':1,'column':4}
     ]
 
+descTile = [
+    {'name':'Docking Hatch #1','room':0,'row':0,'column':1},
+    {'name':'Docking Hatch #2','room':0,'row':2,'column':1},
+
+    {'name':'Toilet','room':2,'row':0,'column':0},
+    
+    {'name':'Water Processor','room':5,'row':1,'column':0},
+    {'name':'Water Processor','room':5,'row':2,'column':0},
+    {'name':'Toilet','room':5,'row':3,'column':0},
+    {'name':'Air Filter','room':5,'row':1,'column':2},
+    ]
+
+#Item 35 is used for lockers that never contain an item, it is therefore set to true.
 item_list = [
 {'ID':0,'name':'Wetsuit','taken':False},
 {'ID':1,'name':'Headset','taken':False},
@@ -143,25 +166,63 @@ item_list = [
 {'ID':32,'name':'Aliens','taken':False},
 {'ID':33,'name':'Iron Man','taken':False},
 {'ID':34,'name':'JW: Parabellum','taken':False},
-{'ID':35,'name':'','taken':False},
+{'ID':35,'name':'Nothing','taken':True},
 {'ID':36,'name':'','taken':False},
 {'ID':37,'name':'','taken':False},
 {'ID':38,'name':'','taken':False},
 {'ID':39,'name':'','taken':False}
 ]
 
+strBlair1=[
+    '*Alarms Blaring*',
+    'You wake up on the floor',
+    '*The alarms stop*',
+    'Primary power is down.\nThe station is running on backup systems.',
+    'You can barely see around you\nThe emergency lights are weak',
+    '[What the hell happened?]',
+    'You remember coming to the medbay...',
+    'and the base moved off foundation...',
+    'you got knocked to the ground\nand bumped your head on the way down.',
+    '[I need to get to the CIC...\nI need to find the others]',
+    '[I need to find answers...]',
+    'The air is stating to feel stale...',
+    'You don\'t have much time'
+    ]
+
 #Game Control Variables
-player_Pos = {'row':0,'column':0}
-currentRoom = 0
+player_Pos = {'row':2,'column':4}
+currentRoom = 2
 map_Current = map_Medbay
 stay = False #For staying in the Room
 continueGame = True
 doorMasterCode = '5389'
 
 #Functions and Classes
-#Map Functions
+#Rendering
 def Map_Display(map):
     '''Map_Display(map):
+    map: Must be a 2-D np array of integers (Room map)
+    This function is for displaying the map state of the current room.
+    Current Issues:
+    Map is distorted when Tile ID is more than 1 digit
+    '''
+    map_dimensions = map.shape
+    #print(map_dimensions)
+    s_map = ("_"*((map_dimensions[1]*2)+1))+"\n"
+    for i in range(0,map_dimensions[0]):
+        s_map = s_map+"|"
+        for j in range(0,map_dimensions[1]):
+            if((player_Pos['row']==i)and(player_Pos['column']==j)):
+                s_map = s_map+'P|'
+            else:
+                s_map = s_map+str(' ')+"|"
+        s_map = s_map+"\n"
+        #s_map = s_map+('-'*((map_dimensions[1]*2)+1))+"\n"
+    s_map = s_map+(chr(8254)*((map_dimensions[1]*2)+1))+"\n"
+    print(s_map)
+
+def Debug_Map_Display(map):
+    '''Debug_Map_Display(map):
     map: Must be a 2-D np array of integers (Room map)
     This function is for displaying the map state of the current room.
     Current Issues:
@@ -181,6 +242,17 @@ def Map_Display(map):
     s_map = s_map+(chr(8254)*((map_dimensions[1]*2)+1))+"\n"
     print(s_map)
 
+# define our clear function
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+ 
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    print('')
+
+
 #Player Input Handlers
 def Input_Handler(char):
     '''Input_Handler(char)
@@ -192,6 +264,7 @@ def Input_Handler(char):
     s: Move down
     d: Move right
     e: Use
+    i: Inventory
     '''
     if (char=='w'):
         Movement_Controller(map_Current,'w')
@@ -203,6 +276,8 @@ def Input_Handler(char):
         Movement_Controller(map_Current,'d')
     elif(char=='e'):
         Use_Handler(map_Current)
+    #elif(char=='i'):
+    #    Inventory()
     else:
         print('Unknown character')
 
@@ -244,13 +319,12 @@ def Use_Handler(map):
         print("You found something on the floor")
         itemFloorHandler()
     elif(tile==4):
-        print("You found a locker")
         storageHandler()
     elif(tile==5):
-        print("You found a safe")
         safeHandler()
     elif(tile==6):
-        print("You found something interesting")
+        #print("There is something here")
+        descHandler()
     elif(tile==7):
         print("You found a Computer")
     elif(tile==8):
@@ -265,8 +339,13 @@ def Use_Handler(map):
     elif(tile==11):
         print("You found an escape pod")
         Triton()
+    hold = msvcrt.getwch() #Only for windows, uncomment if using clearConsole()
 
-#Door Functions
+def Inventory():
+    '''Inventory()
+    '''
+
+#Door Functions (Tile ID=1)
 def doorHandler():
     '''doorHandler()
     This function is used to decide which door needs to be handled.
@@ -351,7 +430,7 @@ def changeRoom(doorNo, invert):
         player_Pos['column'] = doors[doorNo]['t_y'];
     stay = False
 
-#Terminal Functions
+#Terminal Functions (Tile ID=2)
 def terminalHandler():
     '''terminalHandler()
     This function is used to decide which terminal needs to be handled.
@@ -454,7 +533,7 @@ def terminalInterface(doorNo):
                 print("Enter Password:")
     doors[doorNo]['locked']=locked
 
-#Item Floor Functions
+#Item Floor Functions (Tile ID=3)
 def changeTile(map, tileID, row, column):
     map[row][column]=tileID
 
@@ -474,7 +553,7 @@ def itemFloorHandler():
     print('Item Found: '+item_list[itemNo]['name'])
     changeTile(map_Current, 0, player_Pos['row'],player_Pos['column'])
 
-#Storage Functions
+#Storage Functions (Tile ID=4)
 def storageHandler():
     '''storageHandler()
 
@@ -538,13 +617,20 @@ def getStorageItem(storageNo):
         print('There is nothing else here.')
         return
     print('There is something else here.')
-    print('Search?')
-    take = msvcrt.getwch() #Only for windows
-    if(take=='y'):
-        item_list[storage[storageNo]['itemID']]['taken'] = True
-        print('Item Found: '+item_list[storage[storageNo]['itemID']]['name'])
+    print('Search?y/n')
+    while True:
+        take = msvcrt.getwch() #Only for windows
+        if(take=='y'):
+            item_list[storage[storageNo]['itemID']]['taken'] = True
+            print('Item Found: '+item_list[storage[storageNo]['itemID']]['name'])
+            break;
+        elif(take=='n'):
+            print('You left the item behind.')
+            break;
+        else:
+            print('Unknown character!')
 
-#Safe Functions
+#Safe Functions (Tile ID=5)
 def safeHandler():
     getSafeItem(0)
 
@@ -587,35 +673,134 @@ def getSafeItem(safeNo):
     safes[safeNo]['locked']=locked
     print('There is something else here.')
     print('Search?')
-    take = msvcrt.getwch() #Only for windows
-    if(take=='y'):
-        item_list[safes[safeNo]['itemID']]['taken'] = True
-        print('Item Found: '+item_list[safes[safeNo]['itemID']]['name'])
+    while True:
+        take = msvcrt.getwch() #Only for windows
+        if(take=='y'):
+            item_list[safes[safeNo]['itemID']]['taken'] = True
+            print('Item Found: '+item_list[safes[safeNo]['itemID']]['name'])
+            break;
+        elif(take=='n'):
+            print('You left the item behind.')
+            break;
+        else:
+            print('Unknown character!')
+
+#Tile w/Desc (Tile ID=6)
+def descHandler():
+    if(currentRoom==0):
+        if(player_Pos['row']==0):
+            tileNo=0
+        else:
+            tileNo=1
+    elif(currentRoom==2):
+        tileNo=2
+    else:
+        if(player_Pos['row']==1):
+            if(player_Pos['column']==0):
+                tileNo=3
+            else:
+                tileNo=6
+        elif(player_Pos['row']==2):
+            tileNo=4
+        else:
+            tileNo=5
+    print('You Found: '+descTile[tileNo]['name'])
+
+#Computer (Tile ID=7)
 
 #NPCs
+#(Tile ID=8)
 def Doris():
     '''Function for the NPC Doris'''
-    
+    print(f"{'Test' : >150}")
+
+#(Tile ID=9)    
 def Ellen():
     '''Function for the NPC Ellen'''
 
+#(Tile ID=10)
 def Nessie():
     '''Function for the NPC N.E.S.S.I.E'''
 
+def Blair():
+    '''Function for the NPC Doris'''
+    for dialogue in strBlair1:
+        delay_print(dialogue)
+        #print(f"{test3 : >120}")
+        next = msvcrt.getwch() #Only for windows
+        clearConsole()
+
 #Triton
+#(Tile ID=11)
 def Triton():
+    '''
+    This function is used for running the logic to finish the game.
+    '''
     global stay, continueGame
     print('You Won!')
     stay = False
     continueGame = False
 
-#Credits
+#Menus
+def Start_Screen():
+    global continueGame
+    while True:
+        clearConsole()
+        print(title)
+        print('1: Start Game')
+        print('2: Controls')
+        print('3: Credits')
+        print('4: Exit')
+        char = msvcrt.getwch() #Only for windows
+        if(char=='1'):
+            #print('Starting Game')
+            clearConsole()
+            continueGame = True
+            Blair()
+            break
+        elif(char=='2'):
+            Controls()
+        elif(char=='3'):
+            Credits()
+        elif(char=='4'):
+            print('Exiting')
+            continueGame = False
+            break
+        else:
+            print('Unknown Character')
+
 def Credits():
+    clearConsole()
     print('CREDITS')
+    print(title)
     print(credits)
+    print('\nPress any key to continue...')
+    hold = msvcrt.getwch() #Only for windows
+
+def Controls():
+    clearConsole()
+    print('''
+    |++++++++++++++++++++++++++++++++++++++++++++++++++++++++|
+    |     ____  ____  ____  ____  ____  ____  ____  ____     |
+    |    ||C ||||O ||||N ||||T ||||R ||||O ||||L ||||S ||    |
+    |    ||__||||__||||__||||__||||__||||__||||__||||__||    |
+    |    |/__\||/__\||/__\||/__\||/__\||/__\||/__\||/__\|    |
+    |                                                        |
+    |++++++++++++++++++++++++++++++++++++++++++++++++++++++++|
+    |                      W: Move up                        |
+    |                      A: Move left                      |
+    |                      S: Move down                      |
+    |                      D: Move right                     |
+    |                      E: Use\Interact                   |                 
+    |++++++++++++++++++++++++++++++++++++++++++++++++++++++++|
+    ''')
+    print('\nPress any key to continue...')
+    hold = msvcrt.getwch() #Only for windows
+
 
 #Main Function/Testing functions
 print('Game Start')
+Start_Screen()
 while continueGame:
     if(currentRoom==0):
         map_Current = map_DockingHub1
@@ -633,10 +818,12 @@ while continueGame:
         map_Current = map_PrepRoom2
     elif(currentRoom==7):
         map_Current = map_BioChem
-    Map_Display(map_Current)
+    clearConsole()
+    Debug_Map_Display(map_Current)
     stay = True
     while (stay):
         input_char = msvcrt.getwch() #Only for windows
         Input_Handler(input_char)
-        Map_Display(map_Current)
-Credits()
+        clearConsole()
+        Debug_Map_Display(map_Current)
+#Credits()
